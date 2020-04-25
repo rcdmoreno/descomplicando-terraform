869a11b6e22543e2e93e597e277df23af431b91b
rcdmoreno <rcdmoreno@gmail.com>
2020-04-25 15:16:10 -0300

    instance

--- ec2.tf
+++ ec2.tf
@@ -10,8 +10,8 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
   count         = 2
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
